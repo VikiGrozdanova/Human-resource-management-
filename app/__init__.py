50cb63a8e00abfeaa52d4593cd0aef8b0edb424b
VikiGrozdanova <67692826+VikiGrozdanova@users.noreply.github.com>
2020-08-16 19:37:05 +0300

    Add files via upload

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,9 @@ from flask_sqlalchemy import SQLAlchemy
 # local imports
 from config import app_config
 
+# update imports
+from flask import Flask, render_template
+
 db = SQLAlchemy()
 login_manager = LoginManager()
 
@@ -36,6 +39,20 @@ def create_app(config_name):
 
     from .home import home as home_blueprint
     app.register_blueprint(home_blueprint)
+
+    @app.errorhandler(403)
+    def forbidden(error):
+        return render_template('errors/403.html', title='Forbidden'), 403
+
+    @app.errorhandler(404)
+    def page_not_found(error):
+        return render_template('errors/404.html', title='Page Not Found'), 404
+
+    @app.errorhandler(500)
+    def internal_server_error(error):
+        return render_template('errors/500.html', title='Server Error'), 500
+
+
 
     return app
 
